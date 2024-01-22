4ffe3bf8b81c490757c48dce5914b95499dc0bb3
tianjinwu <tianjinwu@nagain.com>
2024-01-22 14:29:39 +0800

    使用iframe嵌套今日热榜页面

---
+++ src/views/hotlist/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <iframe
+      ref="myIframe"
+      frameborder="0"
+      scrolling="auto"
+      :title="11111"
+      src="http://localhost:5173/#/"
+      style="width: 100%; height: 1000px;"
+      @error="handleError"
+    />
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    handleError(event) {
+      console.log('加载异常', event.target.src)
+    }
+  }
+}
+</script>
+
