061482b8e54a4ba10288a0b8f16799690cca5a12
tianjinwu <tianjinwu@nagain.com>
2024-01-31 10:54:16 +0800

    iframe页面宽高自定义显示

--- src/views/hotlist/index.vue
+++ src/views/hotlist/index.vue
@@ -1,31 +1,79 @@
 <template>
   <iframe
+    id="nestIframe"
     ref="iframe"
     :src="url"
     frameborder="0"
     scrolling="auto"
-    style="width: 100%; height: 1000px;"
-    @load="handleLoad"
+    class="content-iframe"
   />
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
+  name: 'Iframe',
   data() {
     return {
       url: 'https://weekly.tw93.fun/'
       // url: 'http://localhost:5173/#/'
     }
+  },
+  computed: {
+    ...mapState({
+      sidebar: state => state.app.sidebar.opened
+    })
+  },
+  // 监听侧栏状态变化
+  watch: {
+    'src.url'(newValue, oldValue) {
+      setTimeout(() => {
+        const iframe = this.$refs.iframe
+        if (newValue !== undefined && newValue !== null && iframe !== undefined) {
+          iframe.contentWindow.location.replace(newValue)
+        }
+      }, 50)
+    },
+    sidebar() {
+      this.changeNestIframe()
+    }
+  },
+  mounted() {
+    this.changeNestIframe()
+    window.onresize = function() { this.changeNestIframe() }
+  },
+  methods: {
+    changeNestIframe() {
+      /**
+       * iframe-宽高自适应显示
+      */
+      const nestIframe = document.getElementById('nestIframe')
+      const deviceWidth = document.body.clientWidth
+      const deviceHeight = document.body.clientHeight
+
+      // 判断侧栏状态是否展开
+      const widthDiff = this.sidebar ? 200 : 0 // 数字是页面布局宽度差值
+      nestIframe.style.width = Number(deviceWidth - widthDiff) + 'px'
+      nestIframe.style.height = Number(deviceHeight) + 'px' // 数字是页面布局高度差
+
+      // console.log('deviceWidth', Number(deviceWidth - widthDiff) + 'px')
+      // console.log('deviceHeight', Number(deviceHeight) + 'px')
+    }
   }
-  // mounted() {
-  //   this.$refs.iframe.onload = () => {
-  //     if (this.$refs.iframe) {
-  //       console.log('iframe 加载完成', this.$refs.iframe)
-  //     } else {
-  //       console.log('iframe 加载失败')
-  //     }
-  //   }
-  // }
 }
+
 </script>
 
+<style scoped>
+.content-iframe {
+  position:absolute;
+  top:0px;
+  left: 0px;
+  right:0px;
+  bottom:100px;
+  width: 100%;
+  height: 100%;
+  min-height: 600px
+}
+</style>
